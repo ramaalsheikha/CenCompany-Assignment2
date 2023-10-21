3dba6cd03784dce97ff999898a496f092f530d59
rama <alsheikharama@gmail.com>
2023-10-22 02:00:58 +0300

    Finish the process of changing the background and color of the coffee options button

--- app/src/main/java/com/example/myapplication/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/ui/activity/MainActivity.kt
@@ -1,16 +1,13 @@
-package com.example.myapplication
+package com.example.myapplication.ui.activity
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import com.example.myapplication.R
 import com.example.myapplication.databinding.ActivityMainBinding
-import com.example.myapplication.ui.OrderBuildingActivity
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding:ActivityMainBinding
-    private val startButton:Button = binding.startBtn
     override fun onCreate(savedInstanceState: Bundle?) {
         binding= ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
@@ -23,8 +20,8 @@ class MainActivity : AppCompatActivity() {
     }
     fun start(){
 
-       startButton.setOnClickListener {
-           startActivity(Intent(this,OrderBuildingActivity::class.java))
+       binding.startBtn.setOnClickListener {
+           startActivity(Intent(this, OrderBuildingActivity::class.java))
        }
 
     }
