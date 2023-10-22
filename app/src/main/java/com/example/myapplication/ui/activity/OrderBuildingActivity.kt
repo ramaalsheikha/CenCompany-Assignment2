d290d2f6830493f36a7c0b2f174c286200985aac
rama <alsheikharama@gmail.com>
2023-10-22 21:23:59 +0300

    The coffee options were shown on the first screen

--- app/src/main/java/com/example/myapplication/ui/activity/OrderBuildingActivity.kt
+++ app/src/main/java/com/example/myapplication/ui/activity/OrderBuildingActivity.kt
@@ -2,15 +2,25 @@ package com.example.myapplication.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.example.myapplication.R
 
 class OrderBuildingActivity : AppCompatActivity() {
-
+    private val optionsFragment = CoffeeOptionsFragment()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_order_biulding)
+        showCoffeeOptionsFragment()
 
         var action = supportActionBar
         action!!.title = "Order"
+    }
+    private fun showCoffeeOptionsFragment(){
+        addFragment(R.id.coffee_options_fragment,optionsFragment)
+    }
+    private fun addFragment(id:Int,fragment:Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.add(id,fragment)
+        transaction.commit()
     }
 }
