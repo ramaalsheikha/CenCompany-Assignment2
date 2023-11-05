cda29928cb93d0d5c98cbb80d32d71515a4d1717
rama <alsheikharama@gmail.com>
2023-11-05 23:39:22 +0300

    Creat Adapter for recyclerView in the last screen

---
+++ app/src/main/java/com/example/myapplication/adapter/RecyclerViewAdapter.kt
@@ -0,0 +1,20 @@
+package com.example.myapplication.adapter
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.myapplication.domine.ItemsViewModel
+
+class RecyclerViewAdapter(private val list:List<ItemsViewModel>):RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+}
